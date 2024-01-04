d04efe1288831f6a006fbf9647a1ded3e141fad5
cowboyliao <2730647052@qq.com>
2024-01-04 10:40:38 -0500

    day1

--- Cpractice/main.c
+++ Cpractice/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(){
-  /* my first c program */
-  printf("hello,world!\n" );
+#include "math_operations.h"
+
+int main() {
+  int a = 10, b = 5;
+
+  printf("Addition: %d\n", add(a, b));
+  printf("Subtraction: %d\n", subtract(a, b));
+  printf("Multiplication: %d\n", multiply(a, b));
+  printf("Division: %d\n", divide(a, b));
+
+  return 0;
 }
