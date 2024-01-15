eeab716ced725c6fb68b17c1bbaff4677d31ecc0
cowboyliao <2730647052@qq.com>
2024-01-14 19:11:31 -0500

    csapp lab1

---
+++ csapp/lab/datalab-handout/shell.nix
@@ -0,0 +1,9 @@
+# with (import <nixpkgs> {}).pkgsi686Linux; 
+with import <nixpkgs> {}; 
+mkShell {
+  NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+  ];
+  NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
+  buildInputs = [ pkgs.pkgsi686Linux]; 
+  # buildInputs = [ pkgs.glibc ]; 
+}
--- flake.nix
+++ flake.nix
@@ -30,9 +30,6 @@
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [ 
-            #lib
-            openssl 
-
             #tools
             gdb
             gnumake
